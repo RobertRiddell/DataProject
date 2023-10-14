170a32171b4f1430cc24fac82d0ed9488dc5806e
Robert Riddell <robriddell@y7mail.com>
2023-10-15 09:13:31 +1100

    More analysis and added report and slide deck

--- funs/gg_box.R
+++ funs/gg_box.R
@@ -1,6 +1,9 @@
 gg_box <-  function(data, feature) {
   ggplot(data, aes(x = .data[[feature]], y = Price)) +
     geom_boxplot(colour = '#145369', fill = '#2596be') + 
-    ggtitle(deparse(substitute(data)) , feature) +
-    theme_classic()
+    ggtitle(deparse(substitute(data)) , feature) + 
+    theme_classic() +
+    theme(axis.text.x = element_text(angle=45, hjust = 1, vjust = 1),
+          plot.title = element_text(hjust = 0.5))
+    
 }
--- funs/gg_hist.R
+++ funs/gg_hist.R
@@ -3,5 +3,7 @@ gg_hist <-  function(data, feature) {
   ggplot(data, aes(x = .data[[feature]])) +
     geom_histogram(colour = '#064470', fill = '#063970', alpha = 0.8, binwidth = binwidth) + 
     ggtitle(deparse(substitute(data)) , feature) +
-    theme_classic()
+    theme_classic() +
+    theme(plot.title = element_text(hjust = 0.5))
+
 }
